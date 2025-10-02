45ec157f26b13f4642b6b094e21de84b13d4e281
Danny <nguyentai19081991@gmail.com>
2025-10-02 16:50:50 +0700

    thay đổi css

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.main')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Account Login') }}</div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('account.login') }}">
-                        @csrf
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-                            <div class="col-md-6">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card register-card">
+                    <div class="card-header register-header">
+                        <h3>{{ __('Account Login') }}</h3>
+                    </div>
+                    <div class="card-body p-4"> <!-- Padding giống register -->
+                        <form method="POST" action="{{ route('account.login') }}">
+                            @csrf
+                            
+                            <!-- E-Mail Address -->
+                            <div class="form-group mb-3">
+                                <label for="email" class="col-form-label">{{ __('E-Mail Address') }}</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-                            <div class="col-md-6">
+
+                            <!-- Password -->
+                            <div class="form-group mb-3">
+                                <label for="password" class="col-form-label">{{ __('Password') }}</label>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -30,9 +32,9 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+
+                            <!-- Remember Me -->
+                            <div class="form-group mb-3">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                     <label class="form-check-label" for="remember">
@@ -40,18 +42,17 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+
+                            <div class="form-group mb-0 text-center">
+                                <button type="submit" class="btn btn-primary submit-btn"
+                                style="width: 100px; height: auto;">
                                     {{ __('Login') }}
                                 </button>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
