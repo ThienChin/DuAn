0c788b9ac74f3a765b3f99a1d2571e06808bf550
ThienChin <Thiendz362@gmail.com>
2025-10-07 15:58:19 +0700

    update

--- resources/views/emails/contact-message.blade.php
+++ resources/views/emails/contact-message.blade.php
@@ -5,7 +5,7 @@
     <title>New Contact Message from {{ $data['name'] }}</title>
 </head>
 <body style="font-family: Arial, sans-serif; line-height: 1.6;">
-    <h2 style="color:#333;">New contact message from {{ $data['name'] }}</h2>
+    <h2 style="color:#333;">Tin nhắn mới từ {{ $data['name'] }}</h2>
 
     <p><strong>Name:</strong> {{ $data['name'] }}</p>
     <p><strong>Email:</strong> {{ $data['email'] }}</p>
